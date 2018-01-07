2ed7ec77802cf99cdb20ff3880714e6392905600
Mark Phillips <work-mpconsultants@outlook.com>
2018-01-07 21:00:37 +0000

    Code refactoring

--- src/charts/utils/chartbuilder.js
+++ src/charts/utils/chartbuilder.js
@@ -1,38 +1,41 @@
 import * as d3 from 'd3';
 import buildPredictionData from './predictionHandler';
 
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
 export default function build(node, data) {
   if (_.isEmpty(data)) {
     return node;
   }
-  const margin = {
-    top: 20,
-    right: 20,
-    bottom: 30,
-    left: 50,
-  };
-
-  const width = 960 - margin.left - margin.right;
-  const height = 500 - margin.top - margin.bottom;
-
-  // Scales
+  /** Scales */
   const yearRange = d3.extent(data[0].values, d => d.year);
-  /** Add additional year to handle prediction */
+  // Add additional year to handle prediction
   yearRange[1] += 1;
-  const xScale = d3.scaleLinear().domain(yearRange).range([0, width]);
-
-  /** Max popularity will be based on the highest prediction */
+  // Max popularity will be based on the highest prediction
   const maxPopularity = _(data)
     .map('prediction')
     .flatten()
     .maxBy('high').high;
 
+  const xScale = d3.scaleLinear().domain(yearRange).range([0, width]);
   const yScale = d3.scaleLinear()
     .domain([0, maxPopularity])
     .range([height, 0]);
 
-  const svg = d3
-    .select(node)
+  /** Axis */
+  const xAxis = d3.axisBottom(xScale).ticks(4).tickFormat(d3.format('d'));
+  const yAxis = d3.axisLeft(yScale);
+
+  /** Chart set up */
+  const svg = d3.select(node)
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
@@ -40,24 +43,17 @@ export default function build(node, data) {
 
   const line = d3.line().x(d => xScale(d.year)).y(d => yScale(d.popularity));
 
-  const lines = svg.append('g').attr('class', 'lines');
-
   // gridlines in x axis function
-
-  function buildXgridlines() {
-    return d3.axisBottom(xScale).ticks(4);
-  }
+  const xGridlines = () => d3.axisBottom(xScale).ticks(4);
 
   // gridlines in y axis function
-  function buildYgridlines() {
-    return d3.axisLeft(yScale);
-  }
+  const yGridlines = () => d3.axisLeft(yScale);
 
-  const lineFunction = d3.line()
+  const predictionLine = d3.line()
     .x(d => xScale(d.x))
     .y(d => yScale(d.y));
 
-  lines
+  svg.append('g').attr('class', 'lines')
     .selectAll('.line-group')
     .data(data)
     .enter()
@@ -68,21 +64,19 @@ export default function build(node, data) {
     .attr('d', d => line(d.values))
     .style('stroke', d => `rgb(${d.rgb.join(',')})`);
 
-  const xAxis = d3.axisBottom(xScale).ticks(4).tickFormat(d3.format('d'));
-  const yAxis = d3.axisLeft(yScale);
 
   // add the X gridlines
   svg.append('g')
     .attr('class', 'grid')
     .attr('transform', `translate(0,${height})`)
-    .call(buildXgridlines()
+    .call(xGridlines()
       .tickSize(-height)
       .tickFormat(''));
 
   // add the Y gridlines
   svg.append('g')
     .attr('class', 'grid')
-    .call(buildYgridlines()
+    .call(yGridlines()
       .tickSize(-width)
       .tickFormat(''));
   svg.append('g')
@@ -117,7 +111,7 @@ export default function build(node, data) {
     .attr('class', 'prediction');
   lineData.forEach((item) => {
     predictionGroup.append('path')
-      .attr('d', lineFunction(item.coordinates))
+      .attr('d', predictionLine(item.coordinates))
       .attr('stroke', `rgb(${item.rgb.join(',')})`)
       .attr('stroke-width', 1)
       .attr('fill', `rgb(${item.rgb.join(',')})`);
