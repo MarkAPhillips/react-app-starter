f61070f5355c8af9deec4eba7495ed036db2591c
Mark Phillips <work-mpconsultants@outlook.com>
2018-02-01 21:45:16 +0000

    Fix build and test

--- __tests__/utils/toHash.spec.js
+++ __tests__/utils/toHash.spec.js
@@ -1,6 +1,10 @@
-// import toHash from '../../src/utils/toHash';
+import toHash from '../../src/utils/toHash';
 
-xdescribe('ToHash specs', () => {
-// TODO:
-
+describe('ToHash specs', () => {
+  it('should convert an array to a hash', () => {
+    const data = [{ id: 1, value: 'Test' }];
+    const result = toHash(data);
+    const expected = { 1: { id: 1, value: 'Test' } };
+    expect(result).toEqual(expected);
+  });
 });
