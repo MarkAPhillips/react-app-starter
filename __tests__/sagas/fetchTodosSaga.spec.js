7502ea695b16c938bb7d789c1e5da1ec21180634
Mark Phillips <work-mpconsultants@outlook.com>
2018-02-02 14:58:14 +0000

    Add additional tests for fetch todos saga

--- __tests__/sagas/fetchTodosSaga.spec.js
+++ __tests__/sagas/fetchTodosSaga.spec.js
@@ -1,6 +1,7 @@
-import { take } from 'redux-saga/effects';
+import { take, call, put } from 'redux-saga/effects';
 import { fetchTodosSaga } from '../../src/sagas';
-import { LOAD_REQUEST } from '../../src/reducers/todoReducer';
+import { LOAD_REQUEST, setTodoItems } from '../../src/reducers/todoReducer';
+import { getTodoItems } from '../../src/services/todoService';
 
 describe('FetchtodosSaga specs', () => {
   const gen = fetchTodosSaga();
@@ -10,6 +11,26 @@ describe('FetchtodosSaga specs', () => {
   it('should wait for the load request to be initiated', () => {
     expected = take(LOAD_REQUEST);
     result = gen.next().value;
+    expect(result).toEqual(expected);
+  });
+
+  it('should call the getTodoItems api call', () => {
+    expected = call(getTodoItems);
+    result = gen.next().value;
+    expect(result).toEqual(expected);
+  });
+
+  it('should set the data returned from a response', () => {
+    const response = { data: [{ id: 1, item: 'Write unit test', completed: false }] };
+    expected = [{ id: 1, item: 'Write unit test', completed: false }];
+    result = gen.next(response).value;
+    expect(result).toEqual(expected);
+  });
+
+  it('should yield action SetTodoItems', () => {
+    const todos = [{ id: 1, item: 'Write unit test', completed: false }];
+    expected = put(setTodoItems(todos));
+    result = gen.next({ todos }).value;
     expect(result).toEqual(expected);
   });
 });
--- src/sagas/fetchTodosSaga.js
+++ src/sagas/fetchTodosSaga.js
@@ -1,6 +1,10 @@
-import { take } from 'redux-saga/effects';
-import { LOAD_REQUEST } from '../reducers/todoReducer';
+import { take, put, call } from 'redux-saga/effects';
+import { LOAD_REQUEST, setTodoItems } from '../reducers/todoReducer';
+import { getTodoItems } from '../services/todoService';
 
 export function* fetchTodosSaga() {
   yield take(LOAD_REQUEST);
+  const response = yield call(getTodoItems);
+  const { todos } = yield response.data;
+  yield put(setTodoItems(todos));
 }
