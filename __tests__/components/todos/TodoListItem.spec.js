c0bf8135a43093049f265b1957723e8db0a35992
Mark Phillips <work-mpconsultants@outlook.com>
2018-02-02 21:12:15 +0000

    Fix  spec for todolistitem

--- __tests__/components/todos/TodoListItem.spec.js
+++ __tests__/components/todos/TodoListItem.spec.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import 'jest-styled-components';
 import { TodoListItem } from '../../../src/components/todos';
 
 describe('TodoListItem specs', () => {
   it('should render the component ', () => {
     const wrapper = shallow(<TodoListItem />);
     expect(wrapper.exists()).toEqual(true);
+  });
+
+  it('should output the item text', () => {
+    const todo = { id: 1, item: 'Write a unit test', completed: false };
+    const wrapper = shallow(<TodoListItem item={todo} />).dive();
+    expect(wrapper.text()).toEqual('Write a unit test');
   });
 });
