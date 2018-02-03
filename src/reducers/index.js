a66df8430cb33198db9717285e75b6730af172f1
Mark Phillips <work-mpconsultants@outlook.com>
2018-02-03 21:18:15 +0000

    Add redux form to app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import error from './errorReducer';
 import todos from './todosReducer';
 
 const rootReducer = combineReducers({
+  form: formReducer,
   error,
   todos,
 });
