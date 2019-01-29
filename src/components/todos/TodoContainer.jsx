b8017521c9c424374f968a6c0d1f5e5a68fe6f9e
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2019-01-29 23:02:15 +0000

    Remove recompose, update babel and reset text input

--- src/components/todos/TodoContainer.jsx
+++ src/components/todos/TodoContainer.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
-import debounceHandler from '@hocs/debounce-handler';
-import preventHandlersDefault from '@hocs/prevent-handlers-default';
-import { compose, withState, withHandlers, lifecycle } from 'recompose';
 import { requestAdd, requestLoad, todosDefaultSelector, requestStatusChange } from '../../reducers/todosReducer';
 import { FORMS } from '../../constants';
 import { addForm, disableForm, formSelector } from '../../reducers/formReducer';
@@ -18,12 +15,13 @@ const propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   handleChange: PropTypes.func.isRequired,
   handleStatusChange: PropTypes.func.isRequired,
+  onLoad: PropTypes.func.isRequired,
   form: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     disabled: PropTypes.bool,
   }),
-  todos: PropTypes.objectOf(PropTypes.shape({
+  todos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string,
     item: PropTypes.string,
     complete: PropTypes.bool,
@@ -35,48 +33,54 @@ const mapStateToProps = state => ({
   form: formSelector(state, formId),
 });
 
-const enhance = compose(
-  connect(mapStateToProps),
-  withState('inputField', 'setInputField', ''),
-  withHandlers({
-    handleSubmit: props => () => {
-      props.dispatch(requestAdd(props.inputField));
-    },
-    handleChange: props => (evt) => {
-      const entry = evt.target.value;
-      props.setInputField(entry);
-      const disabled = !!isEmpty(entry);
-      props.dispatch(disableForm({ id: formId, disabled }));
-    },
-    handleStatusChange: props => (evt) => {
-      const { checked, id } = evt.target;
-      props.dispatch(requestStatusChange({ id, completed: checked }));
-    },
-  }),
-  lifecycle({
-    componentWillMount() {
-      this.props.dispatch(requestLoad());
-      this.props.dispatch(addForm(FORMS[formId]));
-    },
-  }),
-  debounceHandler('handleChange', 300),
-  preventHandlersDefault('handleSubmit'),
-);
+const mapDispatchToProps = dispatch => ({
+  onLoad: () => {
+    dispatch(requestLoad());
+    dispatch(addForm(FORMS[formId]));
+  },
+  handleSubmit: value => dispatch(requestAdd(value)),
+  handleStatusChange: (id, checked) => dispatch(requestStatusChange({ id, completed: checked })),
+  handleChange: (value) => {
+    const disabled = !!isEmpty(value);
+    dispatch(disableForm({ id: formId, disabled }));
+  },
+});
 
-export const Component = ({
-  handleSubmit, handleChange, handleStatusChange, todos, form,
-}) => {
-  const { disabled } = form;
-  return (
-    <TodoContainerPanel>
-      <TodoForm onSubmit={handleSubmit} onChange={handleChange} isDisabled={disabled} />
-      <TodoList todos={todos} onStatusChange={handleStatusChange} />
-      <TodoFilter />
-    </TodoContainerPanel>
-  );
-};
+class TodoContainerComponent extends Component {
+  static propTypes = propTypes;
+  static defaultProps = defaultProps
+  state = {
+    inputField: '',
+  }
+  componentDidMount() {
+    this.props.onLoad();
+  }
+  handleChange = (evt) => {
+    const { value } = evt.target;
+    this.props.handleChange(value);
+    this.setState({
+      inputField: value,
+    });
+  }
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    this.props.handleSubmit(this.state.inputField);
+    this.setState({ inputField: '' });
+  }
+  handleStatusChange = (evt) => {
+    const { checked, id } = evt.target;
+    this.props.handleStatusChange(id, checked);
+  }
+  render() {
+    const { disabled } = this.props.form;
+    return (
+      <TodoContainerPanel>
+        <TodoForm onSubmit={this.handleSubmit} onChange={this.handleChange} isDisabled={disabled} />
+          <TodoList todos={this.props.todos} onStatusChange={this.handleStatusChange} />
+            <TodoFilter />
+      </TodoContainerPanel>
+    );
+  }
+}
 
-Component.propTypes = propTypes;
-Component.defaultProps = defaultProps;
-
-export const TodoContainer = enhance(Component);
+export const TodoContainer = connect(mapStateToProps, mapDispatchToProps)(TodoContainerComponent);
